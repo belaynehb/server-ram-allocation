cd5d784be0a62090faa42ee26cbaf1de8afd14cc
belaynehb <belaynehworku@gmail.com>
2023-01-12 20:32:31 -0500

    First line of code

--- git-demo-file.py
+++ git-demo-file.py
@@ -1,1 +1,7 @@
-test file
+
+TotalMemo = int(input("What is the total memory of the server in GB? "))
+
+if TotalMemo <= 4:
+    print("Reserved memory for the Operating system is:", 1, "GB")
+else:
+    print("I dont' know")
