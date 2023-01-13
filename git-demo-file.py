16f07266d111b38937cd934f32993916f8e8a19f
belaynehb <belaynehworku@gmail.com>
2023-01-12 21:23:05 -0500

    first draft of the project

--- git-demo-file.py
+++ git-demo-file.py
@@ -1,7 +1,33 @@
 
-TotalMemo = int(input("What is the total memory of the server in GB? "))
+# Let us now briefly look at the techniques to size the memory.
 
-if TotalMemo <= 4:
-    print("Reserved memory for the Operating system is:", 1, "GB")
+# 1 GB of memory reserved for Operating System
+# 1 GB each for every 4 GB of RAM after the initial 4 GB, up to 16 GB of RAM
+# 1 GB each for every 8 GB in more than 16 GB of RAM
+
+
+total_memory = int(input("What is the total memory of the server in GB? "))
+
+operating_sys_memory = 0
+sql_server_memory = 0
+if total_memory <= 4:
+    operating_sys_memory = 1
+    sql_server_memory = total_memory - operating_sys_memory
+    print(f"As per best practice recommendation the reserved memory for the Operating system should be : {operating_sys_memory} GB")
+    print(f"As per best practice recommendation the reserved memory for the SQL Server should be: {sql_server_memory} GB")
+elif total_memory > 4 and total_memory <= 16:
+    operating_sys_memory = 1
+    addition = (total_memory - 4) // 4
+    operating_sys_memory += addition
+    sql_server_memory = total_memory - operating_sys_memory
+    print(f"As per best practice recommendation the reserved memory for the Operating system is: {operating_sys_memory} GB")
+    print(f"As per best practice recommendation the reserved memory for the SQL Server should be: {sql_server_memory} GB")
+elif total_memory > 16:
+    operating_sys_memory = 4
+    addition = (total_memory - 16) // 8
+    operating_sys_memory += addition
+    sql_server_memory = total_memory - operating_sys_memory
+    print(f"As per best practice recommendation the reserved memory for the Operating system is: {operating_sys_memory} GB")
+    print(f"As per best practice recommendation the reserved memory for the SQL Server should be: {sql_server_memory} GB")
 else:
-    print("I dont' know")
+    print("Please enter positive value")
